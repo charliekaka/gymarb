059207274a3056215fe75244f3e3206244e85823
charliekaka <70217764+charliekaka@users.noreply.github.com>
2021-12-14 10:53:55 +0100

    14/12

---
+++ pages/register.js
@@ -0,0 +1,12 @@
+const register = () => {
+    return (
+        <div>
+            <form>
+                <imput type="text" placeholder="username"></imput>
+                <imput type="password" placeholder="password"></imput>
+            </form>
+        </div>
+    )
+}
+
+export default register
