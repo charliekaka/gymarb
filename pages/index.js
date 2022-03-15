1e62da0381a02b4cf425ece9923b850de370e5c4
charliekaka <70217764+charliekaka@users.noreply.github.com>
2022-03-15 11:03:01 +0100

    sorting listings

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,72 @@ import { getCookie } from "./api/user/verifyJwt";
 import { getListings } from "./api/listing/fetchListings";
 import { useRouter } from "next/dist/client/router";
 import React, { useState } from "react";
+import Select from "react-select";
 
 export default function Home(props) {
   const router = useRouter();
   const {username, list} = props;
 
-  const [filter, selectFilter] = useState("new")
-  const [listings, renewListings] = useState(renderListings(list, filter))
+  const [listings, renewListings] = useState(renderListings(list));
+
+  // handle listings
+  function renderListings(list, option="new"){
+    if(!list) return;
+
+    console.log(`Rendering ${option} listings...`);
+
+    let listings = [];
+
+    // format listings
+    for(let i = 0; i < list.length; i++){
+      const {user, title, date, price, id} = list[i]
+
+      listings.push(
+        // listing redirects to endpoint of listing id
+        <a key={i} href={`listing/${id}`} className="listingLink">
+          <div className="listingItem" >
+              <div className="itemImageContainer">
+                <img className="itemImage" src="/emptyimage.svg" />
+              </div>
+              <div className="listingTextContainer">
+                <h3 className="itemTitle">{title}</h3>
+                <p className="itemDate">{date.plain}</p>
+                <p className="itemPrice">{price}.€</p>
+                <p className="itemUser">{user} {(user===username)?"(you)":""}</p>
+              </div>
+          </div>
+        </a>
+      )
+    }
+
+      switch (option) {
+        case "new":
+          return listings
+        case "old":
+          // non destructive reverse method
+          return [...listings].reverse()
+        case "low":
+          const prices = [];
+          for(let i = 0; i < listings.length; i++){
+            prices.push(list[i].price)
+          }
+          console.log(prices);
+
+          return
+        case "high":
+          console.log("high");
+          return listings
+      }
+  
+    }
+
+  const options = [
+    {value: "new", label: "New"},
+    {value: "old", label: "Old"},
+    {value: "low", label: "Low pirce"},
+    {value: "high", label: "High price"}
+  ]
+
 
   // render specified content if user is logged im
   function checkIfLoggedIn(username){
@@ -60,55 +119,6 @@ export default function Home(props) {
     }
   }
 
-  // handle listings
-  function renderListings(list, option){
-    if(!list) return;
-
-    let listings = [];
-
-    // format listings
-    for(let i = 0; i < list.length; i++){
-      const {user, title, date, price, id} = list[i]
-
-      listings.push(
-        // listing redirects to endpoint of listing id
-        <a key={i} href={`listing/${id}`} className="listingLink">
-          <div className="listingItem" >
-              <div className="itemImageContainer">
-                <img className="itemImage" src="/emptyimage.svg" />
-              </div>
-              <div className="listingTextContainer">
-                <h3 className="itemTitle">{title}</h3>
-                <p className="itemDate">{date.plain}</p>
-                <p className="itemPrice">{price}.€</p>
-                <p className="itemUser">{user} {(user===username)?"(you)":""}</p>
-              </div>
-          </div>
-        </a>
-      )
-    }
-    // reverse overwrites org array
-    const recersedArray = listings.reverse()
-
-    switch (option) {
-      case "new":
-        console.log("new");
-        return recersedArray
-      case "old":
-        console.log("old");
-        return listings
-      case "low":
-        console.log("low");
-        return
-      case "high":
-        console.log("high");
-        return
-      default:
-        return listings
-    }
-
-  }
-
   // main render
   return (
     <main className="app">
@@ -118,34 +128,19 @@ export default function Home(props) {
           {username ? <h3 className="welcomeMessage">{username}</h3>:""}
           <div className="headerButtonContainer">
             {checkIfLoggedIn(username)}
+
           </div>
           <div>
           </div>
         </header>
 
         <div className="secondHeader">
-          <label htmlFor="selector" className="sort-by">Sort by:</label>
-          <select id="selector" className="sort-selector">
-            <option onClick={()=>{
-              selectFilter("new")
-              renewListings(renderListings(list, filter))
-            }}>New</option>
-            <option onClick={()=>{
-              selectFilter("old")
-              renewListings(renderListings(list, filter))
-
-            }}>Old</option>
-            <option onClick={()=>{
-              selectFilter("low")
-              
-            }}>Low price</option>
-            <option onClick={()=>{
-              selectFilter("high")
-              
-            }}>High price</option>
-          </select>
+          <label className="sort-by">Sort by:</label>
+          <Select instanceId={"selector"} options={options} placeholder={"New"} onChange={(e)=>{
+            renewListings(renderListings(list, e.value))
+          }} />
         </div>
-
+        
         <div className="listingItemContainer">
           {listings}
         </div>
