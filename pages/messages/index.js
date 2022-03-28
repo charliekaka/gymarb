749cb8f31365a938ecf5f9e3e6a866858526d78d
charlie <charliekarlsson112@gmail.com>
2022-03-28 22:36:47 +0200

    chat stuff

--- pages/messages/index.js
+++ pages/messages/index.js
@@ -5,28 +5,47 @@ import { getCookie } from "../api/user/verifyJwt";
 import { handleChat } from "../api/chat/get"
 import Compose from "./Compose";
 
+
 const messages = (props)=>{
     const {username, chats} = props;
 
     const [chat, setChat] = useState(Compose(username))
     
-    function formatChats(items){
+    function formatContacts(items){
         // react renderable elements
-        let chatElements = [];
+        let contacts = [];
         
-        // format chats
+        // format contacts
         for(let i = 0; i < items.length; i++){
             const {users, logs} = items[i];
 
-            chatElements.push(
-                    <div key={i} className={styles.contactItem}>
+            // format chat logs
+            const {time, sender, content} = logs[0];
+            const formatLogs = <div className={styles.chatLogs}>
+                                    <p className={styles.chatLogsSub}>{time}</p>
+                                    <p className={styles.chatLogsSub}>{sender}:</p>
+                                    <p>{content}</p>
+                                </div>
+
+            // get the other user
+            const other = (users[0]===username)?users[1]:users[0];
+
+            // array of renderable content
+            contacts.push(
+                    <li key={i} className={styles.contactItem} onClick={(e)=>{
+                        setChat(formatLogs)
+                    }}>
                         <p className={styles.contactItemText}>
-                            {(users[0]===username)?users[1]:users[0] /*returns the other user*/}
+                            {other}
                         </p>
-                    </div>
+                    </li>
             )
         }
-        return chatElements
+        return contacts
+    }
+
+    function formatChat(chat){
+
     }
     
 
@@ -38,18 +57,27 @@ const messages = (props)=>{
                 width={70}
                 height={70}/>
             </a>
+
             <div className={styles.contentContainer}>
                 <div className={styles.contacts}>
-                    <div className={styles.contactItem}>
+                    <li className={styles.contactItem}>
                         <p className={styles.contactItemText}>
-                            Compose new message!
+                            Create mew
                         </p>
-                    </div>
-                    {formatChats(chats)}
+                    </li>
+                    {formatContacts(chats)}
                 </div>
 
                 <div className={styles.content}>
                     {chat}
+                    
+                    <div className={styles.messageBoxContainer}>
+                        <div className={styles.messageBox}>
+                            yepe
+                        </div>
+                </div>
+            
+
                 </div>
             </div>
         </div>
